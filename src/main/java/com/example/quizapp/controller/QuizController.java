34be608a345c3aef04616b4e7ee1eb9cb031b762
ORSAC <orsac@Mousumis-Mac-mini-670.local>
2023-07-28 18:15:47 +0530

    QuizApp

---
+++ src/main/java/com/example/quizapp/controller/QuizController.java
@@ -0,0 +1,25 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuizController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestion(){
+
+        return questionService.getAllQuestions();
+
+    }
+}
